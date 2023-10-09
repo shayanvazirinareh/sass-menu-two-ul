a681599d77ef2ddfd357395a50e7f8ed7fcf271d
SHAYAN <135424349+shayanvazirinareh@users.noreply.github.com>
2023-10-09 10:32:30 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let BtnHumber=document.querySelector('.menu-humberger');
+let body=document.querySelector('.body');
+let ulHeader=document.querySelector('.ul-header');
+let slider=document.querySelector('.slider');
+let flag=false;
+BtnHumber.addEventListener('click',()=>{
+    if(!flag){
+        flag=true;
+        slider.style.right='49%';
+        slider.style.transition='1s';
+        ulHeader.style.right='3%';
+        ulHeader.style.transition='1.1s';
+    }else{
+        flag=false;
+        slider.style.right='-50%';
+        ulHeader.style.right='-50%';
+    }
+
+})
